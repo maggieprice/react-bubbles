1834c4259a764ab941c1afb13e3138eb339ebe4f
Justin Price <mr.price@Justins-iMac.local>
2020-01-31 14:09:18 -0500

    mvp

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 // import axios from "axios";
-// import {  useHistory } from 'react-router-dom';
+import {  useParams } from 'react-router-dom';
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 const initialColor = {
   color: "",
@@ -8,7 +8,7 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors, history }) => {
-  // const { id } = useParams();
+  const { id } = useParams();
   // const history = useHistory();
   console.log(colors);
   const [editing, setEditing] = useState(false);
